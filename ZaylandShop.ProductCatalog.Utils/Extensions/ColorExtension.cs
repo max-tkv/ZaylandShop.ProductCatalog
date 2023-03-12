1895adb9a65b1de4f275142aed49d8bbefa5133d
Терехов Максим Анатольевич <TerehovMA@moffice.akbars.ru>
2023-03-12 14:42:10 +0300

    Changed: Add ColorExtension

---
+++ ZaylandShop.ProductCatalog.Utils/Extensions/ColorExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace ZaylandShop.ProductCatalog.Utils.Extensions;
+
+public static class ColorExtension
+{
+    public static string ConvertToHex(this Color color)
+    {
+        return "#" + color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
+    }
+    
+    public static string ConvertToRGB(this Color color)
+    {
+        return "RGB(" + color.R + "," +color.G + "," + color.B + ")";
+    }
+}
