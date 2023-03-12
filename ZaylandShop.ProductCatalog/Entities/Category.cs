db8edcc2144be097f0246fe7416e905a4ef1a560
Терехов Максим Анатольевич <TerehovMA@moffice.akbars.ru>
2023-03-12 12:56:58 +0300

    Database: Add group to category

--- ZaylandShop.ProductCatalog/Entities/Category.cs
+++ ZaylandShop.ProductCatalog/Entities/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace ZaylandShop.ProductCatalog.Entities;
+﻿using ZaylandShop.ProductCatalog.Lookups;
+
+namespace ZaylandShop.ProductCatalog.Entities;
 
 public class Category : BaseEntity
 {
     public string Name { get; set; }
+    
+    public Group Group { get; set; }
     
     public ICollection<Product> Products { get; set; }
 }
---
+++ ZaylandShop.ProductCatalog/Lookups/Group.cs
@@ -0,0 +1,19 @@
+﻿namespace ZaylandShop.ProductCatalog.Lookups;
+
+public enum Group
+{
+    /// <summary>
+    /// Для мальчиков
+    /// </summary>
+    Boys = 1,
+    
+    /// <summary>
+    /// Для девочек
+    /// </summary>
+    Girls = 2,
+    
+    /// <summary>
+    /// Общие
+    /// </summary>
+    General = 3
+}
