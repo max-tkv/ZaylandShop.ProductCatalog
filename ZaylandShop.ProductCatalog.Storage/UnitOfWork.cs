5d23a621d227931ad307323029d41b9250d26788
Терехов Максим Анатольевич <TerehovMA@moffice.akbars.ru>
2023-03-12 20:21:57 +0300

    Changed: Added Repositories to UnitOfWork

--- ZaylandShop.ProductCatalog.Storage/UnitOfWork.cs
+++ ZaylandShop.ProductCatalog.Storage/UnitOfWork.cs
@@ -1,16 +1,36 @@
 ﻿using Microsoft.EntityFrameworkCore.Storage;
 using ZaylandShop.ProductCatalog.Abstractions;
+using ZaylandShop.ProductCatalog.Entities;
+using ZaylandShop.ProductCatalog.Repositories;
+using ZaylandShop.ProductCatalog.Storage.Repositories;
 
 namespace ZaylandShop.ProductCatalog.Storage;
 
 public class UnitOfWork : IUnitOfWork
 {
     private readonly AppDbContext _dbContext;
+    
     private IDbContextTransaction _transaction;
+    
+    public IRepository<Product> Products { get; }
+    
+    public IRepository<Category> Categories { get; }
+    
+    public IRepository<ProductFile> Files { get; }
+
+    public IRepository<ProductColor> Colors { get; }
+
+    public IRepository<Brand> Brands { get; }
 
     public UnitOfWork(AppDbContext dbContext)
     {
         _dbContext = dbContext;
+        
+        Products = new ProductRepository(_dbContext);
+        Categories = new CategoryRepository(_dbContext);
+        Brands = new BrandRepository(_dbContext);
+        Colors = new ColorRepository(_dbContext);
+        Files = new FileRepository(_dbContext);
     }
 
     public async Task BeginTransactionAsync()
--- ZaylandShop.ProductCatalog/Abstractions/IUnitOfWork.cs
+++ ZaylandShop.ProductCatalog/Abstractions/IUnitOfWork.cs
@@ -1,9 +1,25 @@
-﻿namespace ZaylandShop.ProductCatalog.Abstractions;
+﻿using ZaylandShop.ProductCatalog.Entities;
+using ZaylandShop.ProductCatalog.Repositories;
+
+namespace ZaylandShop.ProductCatalog.Abstractions;
 
 public interface IUnitOfWork : IDisposable
 {
+    public IRepository<Product> Products { get; }
+    
+    public IRepository<Category> Categories { get; }
+    
+    public IRepository<ProductFile> Files { get; }
+
+    public IRepository<ProductColor> Colors { get; }
+
+    public IRepository<Brand> Brands { get; }
+    
     Task BeginTransactionAsync();
+    
     Task CommitTransactionAsync();
+    
     Task RollbackTransactionAsync();
+    
     Task SaveChangesAsync();
 }
